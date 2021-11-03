3692057e46b0d9bfa67d1699c8dfc72853a53b94
MrGharbi <moaminegharbi@gmail.com>
2021-11-03 23:01:31 +0100

    add styles

--- src/components/search-box/search-boc-component.jsx
+++ src/components/search-box/search-boc-component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import './search-box.styles.css';
 
 const SearchBox = ({ placeholder, handleChange }) => (
-    <input
+    <input className ='search'
          type='search' 
          placeholder={placeholder} 
          onChange={handleChange} />
